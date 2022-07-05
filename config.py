f2f4773cde63b3f5cfad68711f7952609aa38067
Aratab <sbkm1307@gmail.com>
2022-07-05 19:09:40 +0700

    Updated configurations

--- config.py
+++ config.py
@@ -1,50 +1,15 @@
-from tkinter.tix import IMAGE
 import torch
 
-DATASET = "VOC"
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-NUM_WORKERS = 2
-BATCH_SIZE = 32
-IMAGE_SIZE = 416
-NUM_CLASSES = 20 
-LEARNING_RATE = 1e-5
-WEIGHT_DECAY = 1e-4
-NUM_EPOCHS = 100
-CONF_THRESHOLD = 0.05
-MAP_IOU_THRESHOLD = 0.5
-NMS_IOU_THRESHOLD = 0.45
-S = [IMAGE_SIZE//32, IMAGE_SIZE//16, IMAGE_SIZE//8]
-PIN_MEMORY = True
-LOAD_MODEL = True
-SAVE_MODEL = False
-CHECKPOINT_FILENAME = "model.pth.tar"
-IMG_DIR = "/images"
-LABEL_DIR = "/labels"
-ANCHORS = [
-    [(0.28, 0.22), (0.38, 0.48), (0.90, 0.78)],
-    [(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)],
-    [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)]    
-] # Rescale menjadi [0..1] (dibagi dengan imaga_size=416)
-
-PASCAL_CLASSES = [
-    "aeroplane",
-    "bicycle",
-    "bird",
-    "boat",
-    "bottle",
-    "bus",
-    "car",
-    "cat",
-    "chair",
-    "cow",
-    "diningtable",
-    "dog",
-    "horse",
-    "motorbike",
-    "person",
-    "pottedplant",
-    "sheep",
-    "sofa",
-    "train",
-    "tvmonitor"
-]
+ANCHORS = [[(0.28, 0.22), (0.38, 0.48), (0.90, 0.78)], # scale: 13
+           [(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)], # scale: 26
+           [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)], # scale: 52
+           ]
+S  = [13, 26, 52]
+IMG_SIZE = 416
+IMG_DIR = "../input/vehicle-detection/Vehicle Detection/images"
+LABEL_DIR = "../input/vehicle-detection/Vehicle Detection/labels"
+BATCH_SIZE = 16
+EPOCHS = 20
+LEARNING_RATE = 1e-4
+TRAIN_SIZE = 500
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
